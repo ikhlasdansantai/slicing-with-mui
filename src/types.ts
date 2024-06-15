fba77e08d72b70bf045dcc27da92cdeba5dfd920
ikhlasdansantai <initesting5@gmail.com>
2024-06-15 21:11:33 +0700

    config: menampung semua types yang dibutuhkan oleh typescript

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface PartnersItem {
+  icon: JSX.Element | any;
+  title: string;
+}
+
+export interface PriceCardProps {
+  title: string;
+  price: string;
+  description: string;
+  benefits: string[];
+}
+
+// config: menampung semua types yang dibutuhkan oleh typescript
