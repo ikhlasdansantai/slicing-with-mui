2f3f832e71f0e1b9fe1297875cc99d93e5d866bc
ikhlasdansantai <initesting5@gmail.com>
2024-06-18 13:07:28 +0700

    refactor: adding new static datas

--- src/datas.tsx
+++ src/datas.tsx
@@ -1,5 +1,6 @@
 import { FacebookOutlined, GTranslate, PlayCircleFilled, ApiOutlined } from "@mui/icons-material";
 import { PartnersItem, PriceCardProps } from "./types";
+import Images from "./constants/Images";
 
 export const navLinks = [
   { label: "Home", path: "/" },
@@ -37,4 +38,28 @@ export const HowItWorksData: string[] = [
   "Sending invoices to client completed projects",
   "Turn off tracker when the client not agree",
   " More great features coming soon",
+];
+
+export const TestimonialsData = [
+  {
+    id: 1,
+    name: "Lizu",
+    message: "Benarrr helped us boosting our clients prospective to get more high quality projects ever.",
+    position: "Senior UI/UX Designer",
+    profile: Images.ayang1,
+  },
+  {
+    id: 2,
+    name: "Leeseo",
+    message: "With great ecosystem Benarrr we were able secure top notch projects from companies",
+    position: "Founder at Bandung Lautan API",
+    profile: Images.ayang2,
+  },
+  {
+    id: 3,
+    name: "Rei",
+    message: "Benarrr helped us boosting our clients prospective to get more high quality projects ever.",
+    position: "Marketing Manager",
+    profile: Images.ayang3,
+  },
 ];
