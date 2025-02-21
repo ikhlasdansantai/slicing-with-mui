966eba7631b20beffb0fc7a9f385e51998f682f2
ikhlasdansantai <initesting5@gmail.com>
2025-02-21 17:14:11 +0700

    feat: Adjust navbar padding for consistency and code formatting

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -6,18 +6,46 @@ export default function Navbar() {
   const router = window.location.pathname;
 
   return (
-    <Box component="nav" display="flex" justifyContent="space-between" alignItems="center" py={4}>
-      <Box component="figure" sx={{ maxWidth: { xs: "10rem", lg: "15rem" } }}>
+    <Box component="nav" display="flex" justifyContent="space-between" alignItems="center" p={4}>
+      <Box
+        component="figure"
+        sx={{
+          maxWidth: { xs: "10rem", lg: "15rem" },
+        }}
+      >
         <img src={Images.logo} alt="logo" className="img" />
       </Box>
-      <Box component="ul" className="nav-item" gap={3} sx={{ display: { xs: "none", md: "flex" } }}>
+      <Box
+        component="ul"
+        className="nav-item"
+        gap={3}
+        sx={{
+          display: { xs: "none", md: "flex" },
+        }}
+      >
         {navLinks.map((link) => (
-          <Box component="li" color={"ascent.dark"} sx={{ fontWeight: router === link.path ? "bold" : "regular", fontSize: "18px", cursor: "pointer" }} key={link.label}>
+          <Box
+            component="li"
+            color={"ascent.dark"}
+            sx={{
+              fontWeight: router === link.path ? "bold" : "regular",
+              fontSize: "18px",
+              cursor: "pointer",
+            }}
+            key={link.label}
+          >
             {link.label}
           </Box>
         ))}
       </Box>
-      <Button variant="contained" size="large" sx={{ bgcolor: "ascent.dark", textTransform: "none" }}>
+      <Button
+        variant="contained"
+        size="large"
+        sx={{
+          bgcolor: "ascent.dark",
+          textTransform: "none",
+        }}
+      >
         Sign In
       </Button>
     </Box>
