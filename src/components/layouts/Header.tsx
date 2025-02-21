0185a57ddcb8436822ac6257510a7c97c54ad57b
ikhlasdansantai <initesting5@gmail.com>
2025-02-21 17:15:57 +0700

    feat: format code

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,16 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <Container maxWidth="xl" sx={{ paddingInline: { xs: "0.125rem", smallPhone: "1.125rem", lg: "0rem" } }}>
+    <Container
+      maxWidth="xl"
+      sx={{
+        paddingInline: {
+          xs: "0.125rem",
+          smallPhone: "1.125rem",
+          lg: "0rem",
+        },
+      }}
+    >
       <Navbar />
     </Container>
   );
